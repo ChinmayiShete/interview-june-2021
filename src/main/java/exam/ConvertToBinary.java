096828f41530ecbd925166955551ade7e17a9d93
ChinmayiShete <69628569+ChinmayiShete@users.noreply.github.com>
2021-06-22 12:28:06 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,21 @@
 package exam;
-
+import java.io.*;
 public class ConvertToBinary {
-
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
-  }
+    int BiNum[]=new int[32];
+    int i=0;
+    while(n>0){
+      BiNum[i]=n%2;
+      n=n/2;
+      i++;
+    }
+    for(int j=i-1;j>=0;j--)
+      System.out.print(BiNum[j]);
 }
+  public static void main(String args[])
+  {
+    toBinary(n);
+  }
